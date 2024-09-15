8ddaa95953e352b673146269fefaacf1a0c4614a
Fr4nkFletcher <135678866+Fr4nkFletcher@users.noreply.github.com>
2024-09-14 22:40:44 -0500

    blog

---
+++ _site/assets/js/data/swconf.js
@@ -0,0 +1,30 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1726369520',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/categories/',
+      
+        '/tags/',
+      
+        '/archives/',
+      
+        '/about/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
