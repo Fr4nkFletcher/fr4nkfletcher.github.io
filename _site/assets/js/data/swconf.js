35459efe1e8fd3d2c06f4673ab5ebf914db9ffbf
Fr4nkFletcher <135678866+Fr4nkFletcher@users.noreply.github.com>
2024-12-30 14:49:05 -0600

    blog post

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1728102198',resources: [
+    cacheName: 'chirpy-1735591723',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
